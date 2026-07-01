c1a7ed5d61a8a15008de1579bfd641d09e05f101
Swarna <blessyswarna11@gmail.com>
2026-07-01 09:39:43 +0530

    Added languagelearning page and TC11

--- src/test/java/org/identifycourses/tests/TC_11_NavigateLanguages.java
+++ src/test/java/org/identifycourses/tests/TC_11_NavigateLanguages.java
@@ -1,4 +1,15 @@
 package org.identifycourses.tests;
 
-public class TC_11_NavigateLanguages {
+import org.testng.annotations.Test;
+import basetest.BaseTest;
+import org.identifycourses.pages.LanguageLearningPage;
+
+public class TC_11_NavigateLanguages extends BaseTest {
+
+    @Test
+    public void navigateToLanguageLearning() {
+        LanguageLearningPage langPage = new LanguageLearningPage(driver);
+        langPage.openLanguageLearning();
+        System.out.println("Navigation to Language Learning page successful");
+    }
 }
