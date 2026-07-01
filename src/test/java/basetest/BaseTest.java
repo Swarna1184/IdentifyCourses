ee555ac04fd82dbe13727212388cba59cb16eb61
MachirajuPranathi <pranathimachiraju@gmail.com>
2026-07-01 10:12:15 +0530

    /BaseTest.java

--- src/test/java/basetest/BaseTest.java
+++ src/test/java/basetest/BaseTest.java
@@ -1,4 +1,25 @@
 package basetest;
 
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import utilities.DriverSetup;
+import utilities.ExtentReportManager;
+
 public class BaseTest {
+
+    protected WebDriver driver;
+    protected static final String BASE_URL = "https://www.coursera.org/";
+
+    @BeforeMethod
+    public void setUp() {
+        driver = DriverSetup.initDriver();
+        driver.get(BASE_URL);
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        ExtentReportManager.flush();
+        DriverSetup.quitDriver();
+    }
 }
