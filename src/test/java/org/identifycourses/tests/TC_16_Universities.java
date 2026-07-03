b708ec3cef81cbfb86ab1fbf6e0fb4347a422a75
Keerthi092 <keerthichekoti9@gmail.com>
2026-07-03 14:57:15 +0530

    After adding all the test case2

--- src/test/java/org/identifycourses/tests/TC_16_Universities.java
+++ src/test/java/org/identifycourses/tests/TC_16_Universities.java
@@ -7,6 +7,7 @@ import org.testng.annotations.Test;
 
 public class TC_16_Universities extends BaseTest {
     UniversitiesPage page;
+
     @Test(priority = 16, description = "TC_16 - Navigate to For Universities")
     public void validateNavigationToForUniversities() {
         page = new UniversitiesPage(driver);
